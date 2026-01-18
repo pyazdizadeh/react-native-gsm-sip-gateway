2e2e4fb33ff614962a5c25e570207ea8725da13a
Mohamad Yazdizadeh <p.yazdizadeh@yahoo.com>
2026-01-18 08:46:18 +0330

    Add stub index.js for react-native-replace-dialer

---
+++ react-native-replace-dialer/index.js
@@ -0,0 +1,9 @@
+// Simple stub export for react-native-replace-dialer
+
+module.exports = {
+  openDialer: function(number = '') {
+    // Replace with real implementation
+    console.log('openDialer called with', number);
+    return Promise.resolve(true);
+  }
+};
